b433b7630ae626926940c48503ab8bf7f75b1ca4
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-09-13 09:04:42 -0500

    add: component NotificationSystem.vue

--- public_src/src/App.vue
+++ public_src/src/App.vue
@@ -1,36 +1,11 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import io from "socket.io-client"
 import TryForm from './components/TryForm.vue'
 import DesktopNotifications from "./components/DesktopNotifications.vue"
-
-const notifications = ref(["1", "2"])
-const socketEndpoint = import.meta.env.VITE_MAIN_SOCKET
-const socket = io(socketEndpoint)
-
-onMounted(() => {
-  const userId = "1"
-  socket.emit("join", userId)
-  socket.on("notification", (data) => {
-    notifications.value.push(data)
-  })
-  fetch(`${socketEndpoint}/api/notification/${userId}`)
-    .then(response => response.json())
-    .then(data => {
-      notifications.value = data
-    })
-})
+import NotificationSystem from "./components/NotificationSystem.vue"
 </script>
 
 <template>
-  <DesktopNotifications></DesktopNotifications>
-  <TryForm></TryForm>
-  <div class="notification-system">
-    <h2>Notis</h2>
-    <ul>
-      <li v-for="notification in notifications" :key="notification.id">
-        {{ notification.message }}
-      </li>
-    </ul>
-  </div>
+    <DesktopNotifications></DesktopNotifications>
+    <TryForm></TryForm>
+    <NotificationSystem></NotificationSystem>
 </template>
---
+++ public_src/src/components/NotificationSystem.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+import { onMounted, ref, onUnmounted } from "vue";
+import { io, Socket } from "socket.io-client"
+
+const props = defineProps({
+    userId: {
+        type: String,
+        required: true
+    }
+})
+
+const notifications = ref<Array<{ id: number, message: string }>>([])
+let socket: Socket
+
+const socketEndpoint = import.meta.env.VITE_MAIN_SOCKET
+
+onMounted(async () => {
+    socket = io(socketEndpoint)
+    socket.on("connect", () => {
+        socket.emit("authenticate", props.userId)
+    })
+
+    socket.on("notification", (data: { message: string }) => {
+        notifications.value.unshift({
+            id: Date.now(),
+            message: data.message
+        })
+    })
+
+    fetchNotifications()
+})
+
+const fetchNotifications = async () => {
+    try {
+        const response = await fetch(`${socketEndpoint}/api/notifications/${props.userId}`)
+        const data = await response.json()
+        notifications.value = data
+    } catch (error) {
+        console.log("fetchNotifications NotificationSystem.vue error:", error)
+    }
+}
+
+onUnmounted(() => {
+    if (socket) {
+        socket.disconnect()
+    }
+})
+</script>
+<template>
+    {{ userId }}
+    <div class="notification-system">
+        <h2>Notis</h2>
+        <ul>
+            <li v-for="notification in notifications" :key="notification.id">
+                {{ notification.message }}
+            </li>
+        </ul>
+    </div>
+</template>
